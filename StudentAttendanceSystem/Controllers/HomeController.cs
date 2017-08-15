da44ea8fceb0752078617b2a637948b3fe6f74c6
SanobarSattar <30503019+SanobarSattar@users.noreply.github.com>
2017-08-15 23:28:51 +0500

    AttendanceSystem
    AttendanceSystem

---
+++ StudentAttendanceSystem/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentAttendanceSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        StudentAttendanceEntities1 db = new StudentAttendanceEntities1();
+        public ActionResult Index()
+        {
+            var Students = db.Students.ToList();
+            ViewBag.Students = Students;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(int[] markedStudentId, DateTime attendanceDate)
+        {
+
+            int[] mark = markedStudentId;
+            var checkdate = db.Attendances.Where(c => c.Date == attendanceDate).FirstOrDefault();
+            if (checkdate == null)
+            {
+                foreach (var id in mark)
+                {
+                    Attendance at = new Attendance();
+                    at.StudentId = id;
+                    at.Date = attendanceDate;
+                    db.Attendances.Add(at);
+                    db.SaveChanges();
+                    TempData["message"] = "Attendance marked for " + attendanceDate.ToString("d");
+                }
+            }
+            else
+            {
+                TempData["message"] = "Sorry Attendance already marked for " + attendanceDate.ToString("d");
+            }
+
+            var Students = db.Students.ToList();
+            ViewBag.Students = Students;
+
+            return View();
+        }
+
+    }
+}
